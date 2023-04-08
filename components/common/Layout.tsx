db819d4026c6d54e0b54e7424256276e40fb5a69
jasonzubiate <jzubiate@uci.edu>
2023-04-08 16:16:06 -0700

    fixed layout

--- components/common/Layout.tsx
+++ components/common/Layout.tsx
@@ -1,7 +1,11 @@
 import Navbar from "@/components/common/Navbar";
 import Footer from "@/components/common/Footer";
 
-export default function Layout({ children }) {
+type LayoutProps = {
+	children: React.ReactNode;
+};
+
+export default function Layout({ children }: LayoutProps) {
 	return (
 		<div>
 			<Navbar />
